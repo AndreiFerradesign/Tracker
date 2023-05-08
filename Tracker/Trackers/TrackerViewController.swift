a5406f2ff41fdc932a65cab2e95a5938e450e272
Andrei <114855224+AndreiFerradesign@users.noreply.github.com>
2023-05-08 17:48:54 +0300

    Добавлен date picker и SearchField

--- Tracker/Trackers/TrackerViewController.swift
+++ Tracker/Trackers/TrackerViewController.swift
@@ -28,6 +28,36 @@ final class TrackerController: UIViewController {
         return button
     }()
     
+    private lazy var datePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        picker.backgroundColor = .white
+        picker.tintColor = .blue
+        picker.datePickerMode = .date
+        picker.preferredDatePickerStyle = .compact
+        picker.locale = Locale(identifier: "ru_RU")
+        picker.calendar = Calendar(identifier: .iso8601)
+        picker.maximumDate = Date()
+        picker.addTarget(self, action: #selector(didChangedDatePicker), for: .valueChanged)
+        return picker
+    }()
+    
+    private lazy var searchBar: UISearchBar = {
+        let view = UISearchBar()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.placeholder = "Поиск"
+        view.searchBarStyle = .minimal
+//        view.delegate = self
+        return view
+    }()
+    
+    // MARK: - Properties
+    
+    private var currentDate = Date.from(date: Date())!
+    private var searchText = ""
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,6 +65,8 @@ final class TrackerController: UIViewController {
         setupConstraints()
         
     }
+    
+    // MARK: - Actions
     
     @objc
     private func didTapPlusButton() {
@@ -44,12 +76,22 @@ final class TrackerController: UIViewController {
         present(navigationController, animated: true)
     }
     
+    @objc
+    private func didChangedDatePicker(_ sender: UIDatePicker) {
+        currentDate = Date.from(date: sender.date)!
+     //   collectionView.reloadData()
+    }
 }
+
+// MARK: - Layout methods
+
 private extension TrackerController {
     func setupContent() {
         view.backgroundColor = .white
         view.addSubview(addButton)
         view.addSubview(titleLabel)
+        view.addSubview(datePicker)
+        view.addSubview(searchBar)
     }
     
     func setupConstraints() {
@@ -60,7 +102,14 @@ private extension TrackerController {
             // titleLabel
             titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             titleLabel.topAnchor.constraint(equalTo: addButton.bottomAnchor, constant: 13),
-
+            // datePicker
+            datePicker.widthAnchor.constraint(equalToConstant: 120),
+            datePicker.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+            datePicker.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            // searchBar
+            searchBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            searchBar.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
         ])
     }
 }
@@ -68,5 +117,27 @@ private extension TrackerController {
 extension TrackerController: AddTrackerViewControllerDelegate {
     func didSelectTracker(with type: AddTrackerViewController.TrackerType) {
         dismiss(animated: true)
+    }
+}
+
+// MARK: - UISearchBarDelegate
+
+extension TrackerController: UISearchBarDelegate {
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.searchText = searchText
+      //  collectionView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.endEditing(true)
+        searchBar.setShowsCancelButton(false, animated: true)
+        self.searchText = ""
+      //  collectionView.reloadData()
     }
 }
