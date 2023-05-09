ab60e46459ce0f97819450e1a65cd92865318366
Andrei <114855224+AndreiFerradesign@users.noreply.github.com>
2023-05-09 23:01:15 +0300

    Create Int+Extentions.swift

---
+++ Tracker/Helpers/Extensions/Int+Extentions.swift
@@ -0,0 +1,18 @@
+//
+//  Int+Extentions.swift
+//  Tracker
+//
+//  Created by Andrei Kashin on 09.05.2023.
+//
+
+extension Int {
+    
+    func days() -> String {
+        var ending: String!
+        if "1".contains("\(self % 10)")      { ending = "день" }
+        if "234".contains("\(self % 10)")    { ending = "дня"  }
+        if "567890".contains("\(self % 10)") { ending = "дней" }
+        if 11...14 ~= self % 100             { ending = "дней" }
+        return "\(self) " + ending
+    }
+}
